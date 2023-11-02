5093cddd74d79a289a86c3a89d6d3e92f0516237
Vinicius <59659214+ZeAndradee@users.noreply.github.com>
2023-11-02 15:36:16 -0300

    opcao1 concluida

--- modulo3.py
+++ modulo3.py
@@ -1,6 +1,7 @@
-class medicamento:
-    def __init__(self, nome, principio_ativo,dosagem,administracao,fabricante, lote, datadevalidade):
+class medicamentoN:
+    def __init__(self, nome,quantidade, principio_ativo,dosagem,administracao,fabricante, lote, datadevalidade):
         self.nome = nome
+        self.quantidade = quantidade
         self.principio_ativo = principio_ativo
         self.dosagem = dosagem
         self.administracao = administracao
@@ -8,7 +9,7 @@ class medicamento:
         self.lote = lote
         self.datadevalidade = datadevalidade
 
-medicamento1 = medicamento("Dorflex", "Dipirona", "500mg", "Oral", "EMS", "123456", "12/02/2023")
+medicamento1 = medicamentoN("Dorflex", 20, "Dipirona", "500mg", "Oral", "EMS", "123456", "12/02/2023")
 
 medicamentos = [
     medicamento1
@@ -16,14 +17,15 @@ medicamentos = [
 
 def menuInicial():
     try:
-        print("\n---- Administração de Medicamentos ----\n\n1. Cadastro de Medicamentos\n2. Visualizar Estoque\n3. Remover Medicamento\n4. Rastreamento de Lotes\n5. Administrar Medicamento\n")
+        print("\n---- Gerenciamento de Medicamentos ----\n\n1. Cadastro de Medicamentos\n2. Visualizar Estoque\n3. Remover Medicamento\n4. Rastreamento de Lotes\n5. Administrar Medicamento\n")
         escolhaMenu = int(input("\nDigite o numero da opcao desejada: "))
 
         match escolhaMenu:
             case 1:
                 adicionarMedicamento()
             case 2:
-                visualizarEstoque()
+                inFuncao = False
+                visualizarEstoque(inFuncao)
             case 3:
                 removerMedicamento()
             case 4:
@@ -37,37 +39,52 @@ def menuInicial():
         print("\nOpção inválida, tente novamente!\n")
         menuInicial()
 
-    def adicionarMedicamento():
-        print("\n---- Cadastro de Medicamentos ----\n")
-        nomeMedicamento = input("Nome do medicamento: ")
-        principioAtivo = input("Principio ativo: ")
-        dosagem = input("Dosagem: ")
-        administracao = input("Administração: ")
-        fabricante = input("Fabricante: ")
-        lote = input("Lote: ")
-        dataDeValidade = input("Data de validade: ")
+def adicionarMedicamento():
+    inFuncao = True
+    visualizarEstoque(inFuncao)
 
-        medicamentoNovo = medicamento(nomeMedicamento, principioAtivo, dosagem, administracao, fabricante, lote, dataDeValidade)
-        medicamentos.append(medicamentoNovo)
-        print("\nMedicamento adicionado com sucesso!\n")
-        menuInicial()
+    print("\n---- Cadastro de Medicamentos ----\n")
 
-    def visualizarEstoque():
-        inFuncao = True
-        count = 1
-        print("\n---- Estoque de Medicamentos ----")
-        for medicamento in medicamentos:
-            print(f"\nMedicamento {count}: {medicamento.nome}\nPrincipio ativo: {medicamento.principio_ativo}\nDosagem: {medicamento.dosagem}\nAdministração: {medicamento.administracao}\nFabricante: {medicamento.fabricante}\nLote: {medicamento.lote}\nData de validade: {medicamento.datadevalidade}\n")
-            count += 1
-        if inFuncao == False:
-            input("\nPressione qualquer tecla para voltar ao menu inicial")    
+    nomeMedicamento = input("Nome do medicamento: ")
+    #Verificando se o medicamento ja existe no estoque
+    for medicamento in medicamentos:
+        if nomeMedicamento == medicamento.nome:
+            print("\nMedicamento já cadastrado!\n")
+            userInput = input("Deseja atualizar a quantidade do medicamento? (S/N): ")
+            if userInput == "S" or userInput == "s":
+                quantidade = int(input("Digite a quantidade que deseja adicionar: "))
+                medicamento.quantidade += quantidade
+                print("\nQuantidade atualizada com sucesso!\n")
+                menuInicial()
+        else:
+            quantidadeMedicamento = int(input("Quantidade: "))        
+            principioAtivo = input("Principio ativo: ")
+            dosagem = input("Dosagem: ")
+            administracao = input("Administração: ")
+            fabricante = input("Fabricante: ")
+            lote = input("Lote: ")
+            dataDeValidade = input("Data de validade: ")
+
+            medicamentoNovo = medicamentoN(nomeMedicamento, quantidadeMedicamento, principioAtivo, dosagem, administracao, fabricante, lote, dataDeValidade)
+            medicamentos.append(medicamentoNovo)
+            print("\nMedicamento adicionado com sucesso!\n")
             menuInicial()
 
-    def removerMedicamento():
-        pass
-    def rastrearLote():
-        pass
-    def administrarMedicamento():
-        pass
+def visualizarEstoque(inFuncao):
+    count = 1
+    print("\n---- Estoque de Medicamentos ----")
+    for medicamento in medicamentos:
+        print(f"\nMedicamento {count}:\n\nNome Comercial: {medicamento.nome}\nQuantidade: {medicamento.quantidade}\nPrincipio ativo: {medicamento.principio_ativo}\nDosagem: {medicamento.dosagem}\nAdministração: {medicamento.administracao}\nFabricante: {medicamento.fabricante}\nLote: {medicamento.lote}\nData de validade: {medicamento.datadevalidade}\n")
+        count += 1
+    if inFuncao == False:
+        input("\nPressione qualquer tecla para voltar ao menu inicial ")    
+        menuInicial()
+
+def removerMedicamento():
+    pass
+def rastrearLote():
+    pass
+def administrarMedicamento():
+    pass
         
 menuInicial()
