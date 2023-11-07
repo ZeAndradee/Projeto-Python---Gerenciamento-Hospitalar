43ca258c82b2689e744d27273c6816ec12613806
Samuel-Duque <samuellocatel@gmail.com>
2023-11-06 23:19:51 -0300

    att

--- entradas.py
+++ entradas.py
@@ -86,8 +86,6 @@ def registrar_horas_trabalhadas(turnos):
     for turno in turnos:
         print(turno.profissional_alocado.nome)
 
-
-
         inicio = int(input("Digite o horario de entrada do funcionario: "))
         fim = int(input("Digite o horario de saida do funcionario: "))
         pausas = int(input("Digite o numero de pausas do funcionario: "))
@@ -95,6 +93,71 @@ def registrar_horas_trabalhadas(turnos):
 
         turno.registrar_horas_trabalhadas(inicio, fim, pausas, horas_extras)
 
+def trocar_plantoes(profissionais, turnos):
+
+    print("Escolha o primeiro profissional para a troca:")
+    for index, prof in enumerate(profissionais):
+        print(index, '.', f"NOME:{prof.nome}\nCARGO:{prof.cargo}\nSTATUS:{prof.status}\n")
+
+    escolha1 = int(input("Escolha o primeiro profissional: "))
+    
+    if escolha1 < 0 or escolha1 >= len(profissionais):
+        print("Escolha inválida.")
+        return
+
+    profissional1 = profissionais[escolha1]
+
+    print("Escolha o turno a ser trocado pelo primeiro profissional:")
+    for index, turno in enumerate(turnos):
+        if turno.profissional_alocado == profissional1:
+            print(index, f"DATA:{turno.data}\nHORÁRIO:{turno.horario}\nCARGA HORÁRIA:{turno.carga_horaria}\n")
+
+    escolha_turno1 = int(input("Escolha o turno a ser trocado: "))
+
+    if escolha_turno1 < 0 or escolha_turno1 >= len(turnos):
+        print("Escolha de turno inválida.")
+        return
+
+    turno1 = turnos[escolha_turno1]
+
+    print("Escolha o segundo profissional para a troca:")
+    for index, prof in enumerate(profissionais):
+        if prof != profissional1:
+            print(index, '.', f"NOME:{prof.nome}\nCARGO:{prof.cargo}\nSTATUS:{prof.status}\n")
+
+    escolha2 = int(input("Escolha o segundo profissional: "))
+    
+    if escolha2 < 0 or escolha2 >= len(profissionais):
+        print("Escolha inválida.")
+        return
+
+    profissional2 = profissionais[escolha2]
+
+    print("Escolha o turno a ser trocado pelo segundo profissional:")
+    for index, turno in enumerate(turnos):
+        if turno.profissional_alocado == profissional2:
+            print(index, f"DATA:{turno.data}\nHORÁRIO:{turno.horario}\nCARGA HORÁRIA:{turno.carga_horaria}\n")
+
+    escolha_turno2 = int(input("Escolha o turno a ser trocado: "))
+
+    if escolha_turno2 < 0 or escolha_turno2 >= len(turnos):
+        print("Escolha de turno inválida.")
+        return
+
+    turno2 = turnos[escolha_turno2]
+
+    # Realize a troca dos turnos entre os profissionais
+    profissional1.remover_turno(turno1)
+    profissional2.remover_turno(turno2)
+
+    profissional1.alocar_turno(turno2)
+    profissional2.alocar_turno(turno1)
+
+    turno1.alocar_profissional(profissional2)
+    turno2.alocar_profissional(profissional1)
+
+    print("Troca de plantões realizada com sucesso!")
+ 
 def main():
     profissionais = []
     turnos = []
@@ -104,7 +167,8 @@ def main():
         print("[1] - Cadastrar profissional")
         print("[2] - Alocação de plantão")
         print("[3] - Registro do plantão")
-        print("[4] - Gerar de escala de trabalho")
+        print("[4] - Trocar plantões")
+        print("[5] - Gerar escala de trabalho")
         
         user_input = input("\nEscolha a opção desejada: ")
 
@@ -112,10 +176,12 @@ def main():
             case '1':
                 cadastro(profissionais)
             case '2':
-                alocar_turno(profissionais,turnos)
+                alocar_turno(profissionais, turnos)
             case '3':
                 registrar_horas_trabalhadas(turnos)
             case '4':
+                trocar_plantoes(profissionais, turnos)
+            case '5':
                 gerar_relatorio(turnos)
 
 
